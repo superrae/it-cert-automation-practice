bd248d0f45c0b6198a56054775491ece60029902
superrae <rahma.aboeldahab2001@gmail.com>
2024-01-17 18:18:15 +0000

    closes: #1 yayayayay

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -15,7 +15,7 @@ def validate_user(username, minlen):
     # Usernames can only use letters, numbers, dots and underscores
     if not re.match('^[a-z0-9._]*$', username):
         return False
-    if username[0] in "012345678_.9":
+    if username[0] in "0126345678_.9":
         return False
     # Usernames can't begin with a number
     if username[0].isnumeric():
